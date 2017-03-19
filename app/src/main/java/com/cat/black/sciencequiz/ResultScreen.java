b8b6dbc795df3a5fa5cbf4c582de81bdc5b1f0fe
BlackCat <kaeshi2@gmail.com>
2017-03-19 11:18:44 +0100

    Add deep links intent filter in manifest, upgrade restart intent on final screen

--- app/src/main/java/com/cat/black/sciencequiz/ResultScreen.java
+++ app/src/main/java/com/cat/black/sciencequiz/ResultScreen.java
@@ -69,7 +69,6 @@ public class ResultScreen extends AppCompatActivity {
     private void returnToHome () {
         Intent i = getBaseContext().getPackageManager()
                 .getLaunchIntentForPackage( getBaseContext().getPackageName() );
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
     }
 
