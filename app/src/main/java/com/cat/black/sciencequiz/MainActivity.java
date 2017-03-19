1b46a4a0eaa0b027ccda13c9f55b96f69d13502a
BlackCat <kaeshi2@gmail.com>
2017-03-19 11:06:39 +0100

    Add correct answer indication in colors, lock quiz screen orientation to port, downgrade gradle to 2.2.3 (emulation+verifier issues)

--- app/src/main/java/com/cat/black/sciencequiz/MainActivity.java
+++ app/src/main/java/com/cat/black/sciencequiz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cat.black.sciencequiz;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Tints Results button while inactive on supported OS
         if (android.os.Build.VERSION.SDK_INT >= 21) {
-            (findViewById(R.id.button_result)).getBackground().setColorFilter(0xFFC5E1A5, PorterDuff.Mode.MULTIPLY);;
+            (findViewById(R.id.button_result)).getBackground().setColorFilter(0xFFC5E1A5, PorterDuff.Mode.MULTIPLY);
         }
     }
 
@@ -51,6 +54,52 @@ public class MainActivity extends AppCompatActivity {
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(layout);
         toast.show();
+    }
+
+    // Tint RadioButton/CheckBox and text to red
+    public void paintRed (int bid) {
+        CompoundButton button = (CompoundButton) findViewById(bid);
+        // Recolor text
+        int red = 0xFFFF6969;
+        button.setTextColor(red);
+        // Recolor button
+        ColorStateList colorState = new ColorStateList(
+                new int[][]{
+                        new int[]{android.R.attr.state_enabled}
+                },
+                new int[] {
+                        red
+                }
+        );
+        if(Build.VERSION.SDK_INT>=21) {
+            button.setButtonTintList(colorState);
+        }
+    }
+
+    // Tint RadioButton/CheckBox and text to green
+    public void paintGreen (int bid) {
+        CompoundButton button = (CompoundButton) findViewById(bid);
+        // Recolor text
+        int green = 0xFF33691E;
+        button.setTextColor(green);
+        // Recolor button
+        ColorStateList colorState = new ColorStateList(
+                new int[][]{
+                        new int[]{android.R.attr.state_enabled}
+                },
+                new int[] {
+                        green
+                }
+        );
+        if(Build.VERSION.SDK_INT>=21) {
+            button.setButtonTintList(colorState);
+        }
+    }
+
+    // Tint card background to red
+    public void tintRed (int cid) {
+        CardView card = (CardView) findViewById(cid);
+        card.setCardBackgroundColor(0xffffebee);
     }
 
     // Lock answer changes method: true = unlocked, false = locked
@@ -106,27 +155,27 @@ public class MainActivity extends AppCompatActivity {
         // Check for correct answers, build score (number of correct answers), highlight cards with errors in red
         score = 0;
         if (((RadioButton) findViewById(R.id.q1_b)).isChecked()) {score += 1;}
-        else {((CardView) findViewById(R.id.question1)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question1); paintRed(R.id.q1_a); paintRed(R.id.q1_c); paintRed(R.id.q1_d); paintGreen(R.id.q1_b);}
         if (((RadioButton) findViewById(R.id.q2_a)).isChecked()) {score += 1;}
-        else {((CardView) findViewById(R.id.question2)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question2); paintRed(R.id.q2_b); paintRed(R.id.q2_c); paintRed(R.id.q2_d); paintGreen(R.id.q2_a);}
         if (((RadioButton) findViewById(R.id.q3_c)).isChecked()) {score += 1;}
-        else {((CardView) findViewById(R.id.question3)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question3); paintRed(R.id.q3_a); paintRed(R.id.q3_b); paintRed(R.id.q3_d); paintGreen(R.id.q3_c);}
         if (((RadioButton) findViewById(R.id.q4_c)).isChecked()) {score += 1;}
-        else {((CardView) findViewById(R.id.question4)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question4); paintRed(R.id.q4_a); paintRed(R.id.q4_b); paintRed(R.id.q4_d); paintGreen(R.id.q4_c);}
         if (((RadioButton) findViewById(R.id.q5_d)).isChecked()) {score += 1;}
-        else {((CardView) findViewById(R.id.question5)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question5); paintRed(R.id.q5_a); paintRed(R.id.q5_b); paintRed(R.id.q5_c); paintGreen(R.id.q5_d);}
         if (((RadioButton) findViewById(R.id.q6_a)).isChecked()) {score += 1;}
-        else {((CardView) findViewById(R.id.question6)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question6); paintRed(R.id.q6_b); paintRed(R.id.q6_c); paintRed(R.id.q6_d); paintGreen(R.id.q6_a);}
         if (((RadioButton) findViewById(R.id.q7_c)).isChecked()) {score += 1;}
-        else {((CardView) findViewById(R.id.question7)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question7); paintRed(R.id.q7_a); paintRed(R.id.q7_b); paintRed(R.id.q7_d); paintGreen(R.id.q7_c);}
         if (((RadioButton) findViewById(R.id.q8_b)).isChecked()) {score += 1;}
-        else {((CardView) findViewById(R.id.question8)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question8); paintRed(R.id.q8_a); paintRed(R.id.q8_c); paintRed(R.id.q8_d); paintGreen(R.id.q8_b);}
         if ((((CheckBox) findViewById(R.id.q9_a)).isChecked()) && (((CheckBox) findViewById(R.id.q9_c)).isChecked()) && !(((CheckBox) findViewById(R.id.q9_b)).isChecked()) && !(((CheckBox) findViewById(R.id.q9_d)).isChecked()))
         {score += 1;}
-        else {((CardView) findViewById(R.id.question9)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question9); paintRed(R.id.q9_b); paintRed(R.id.q9_d); paintGreen(R.id.q9_a); paintGreen(R.id.q9_c);}
         if ((((CheckBox) findViewById(R.id.q10_c)).isChecked()) && (((CheckBox) findViewById(R.id.q10_d)).isChecked()) && !(((CheckBox) findViewById(R.id.q10_a)).isChecked()) && !(((CheckBox) findViewById(R.id.q10_b)).isChecked()))
         {score += 1;}
-        else {((CardView) findViewById(R.id.question10)).setCardBackgroundColor(0xffffebee);}
+        else {tintRed(R.id.question10); paintRed(R.id.q10_a); paintRed(R.id.q10_b); paintGreen(R.id.q10_c); paintGreen(R.id.q10_d);}
         // Lock answer changes after checked
         allowChanges(false);
         // Build toast message (add score), print on screen
